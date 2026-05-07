a41e2906dd0a1ca3f6e45ec5aaea3a18df5a703f
LalithaGandhi <lalithagandhi2001@gmail.com>
2026-05-07 20:46:21 +0530

    Add files via upload

---
+++ Js/music_list.js
@@ -0,0 +1,63 @@
+let allMusic=[
+    {
+        name:"Arjunaru_Villu",
+        artist:"Sukwinder Singh, Manikka vinayagam",
+        img:"vijay",
+        src:"Arjunaru_Villu"
+    },
+    {
+        name:"Ennai_thottu_alli_konda",
+        artist:"SPB, Swarnalatha",
+        img:"logo1",
+        src:"Ennai_thottu_alli_konda"
+    },
+    {
+        name:"Ethana_Saami",
+        artist:"Pushpavanam Kuppusamy",
+        img:"dhanush",
+        src:"Ethana_Saami"
+    },
+    {
+        name:"Ratnamala",
+        artist:"G.V.Prakash",
+        img:"sivakarthikeyan",
+        src:"Ratnamala"
+    },
+    {
+        name:"Lolita",
+        artist:"Karhik, Prashanthini",
+        img:"ravi",
+        src:"Lolita_Hello"
+    },
+    {
+        name:"Vizhi_moodi",
+        artist:"Karthik",
+        img:"surya",
+        src:"Vizhi_Moodi_Yosithaal"
+    },
+    {
+        name:"Thuli_Thuli",
+        artist:"Haricharan",
+        img:"karthi",
+        src:"Thuli_Thuli_Mazhaiyaai"
+    },
+    {
+        name:"Ketta_Kodukkira",
+        artist:"Jessie Gift, Chinmayi, Sujatha, Ganga",
+        img:"vishal",
+        src:"Ketta_Kodukkira_Boomi"
+    },
+    {
+        name:"Kanavellam",
+        artist:"Karthik, Jayachandran",
+        img:"ajith",
+        src:"Kanavellam_Palikudhe"
+    },
+    {
+        name:"Avalum_Naanum",
+        artist:"Vijay Yesudas",
+        img:"simbu",
+        src:"Avalum_Naanum"
+    },
+    
+]
